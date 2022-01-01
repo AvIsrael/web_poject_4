e60bfc5fd91a5188798d6d181dd118a32faa7e46
AvIsrael <mAjgik-guxfos-wicju0>
2022-01-01 13:06:39 +0200

    added styles for form

--- scripts/popup.js
+++ scripts/popup.js
@@ -4,10 +4,31 @@ btnEdit.addEventListener("click", () => {
     popUp.classList.add("popup_opened");
 })
 
-
-
-
 let btnCancel = document.querySelector(".popup__button")
  btnCancel.addEventListener("click", () => {
      popUp.classList.remove("popup_opened");
 })
+
+let currentName = document.querySelector(".profile__hero");
+let currentRole = document.querySelector(".profile__role");
+let formElement = document.querySelector("#formElement");
+
+    function handleProfileFormSubmit(evt) {
+        evt.preventDefault();
+            let nameInput = document.querySelector("#name");
+            let jobInput = document.querySelector("#about");
+        currentName.textContent = nameInput.value;
+        currentRole.textContent = jobInput.value;
+        popUp.classList.remove("popup_opened");
+    }
+formElement.addEventListener('submit', handleProfileFormSubmit);
+
+
+
+btnEdit.addEventListener('click', setInitValue);
+function setInitValue() {
+    let nameInput = document.querySelector("#name");
+    let jobInput = document.querySelector("#about");
+    nameInput.value = currentName.textContent;
+    jobInput.value = currentRole.textContent;
+}
