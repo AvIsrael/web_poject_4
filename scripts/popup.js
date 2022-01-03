971dc42550543acfa08501366f57e9e30e521fc6
AvIsrael <mAjgik-guxfos-wicju0>
2022-01-03 16:58:35 +0200

    fixes after 1st review

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,14 +1,6 @@
 let btnEdit = document.querySelector(".profile__button-unusual")
 let popUp = document.querySelector(".popup");
-btnEdit.addEventListener("click", () => {
-    popUp.classList.add("popup_opened");
-})
-
-let btnCancel = document.querySelector(".popup__button")
- btnCancel.addEventListener("click", () => {
-     popUp.classList.remove("popup_opened");
-})
-
+let btnCancel = document.querySelector(".popup__button");
 let currentName = document.querySelector(".profile__hero");
 let currentRole = document.querySelector(".profile__role");
 let formElement = document.querySelector("#formElement");
@@ -21,14 +13,18 @@ let formElement = document.querySelector("#formElement");
         currentRole.textContent = jobInput.value;
         popUp.classList.remove("popup_opened");
     }
+
 formElement.addEventListener('submit', handleProfileFormSubmit);
-
-
-
-btnEdit.addEventListener('click', setInitValue);
 function setInitValue() {
+    popUp.classList.add("popup_opened");
     let nameInput = document.querySelector("#name");
     let jobInput = document.querySelector("#about");
     nameInput.value = currentName.textContent;
     jobInput.value = currentRole.textContent;
 }
+
+btnCancel.addEventListener("click", () => {
+    popUp.classList.remove("popup_opened");
+})
+
+btnEdit.addEventListener('click', setInitValue);
